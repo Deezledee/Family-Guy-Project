05c4a6ffdf5a2fc3684f9065091b600b4761dee3
Diana Kalstein <dianakalstein@gmail.com>
2023-04-04 17:08:42 +0200

    Added Quagmire, Stewie & Clock

--- js/game.js
+++ js/game.js
@@ -7,18 +7,21 @@ class Game {
       this.quagmireSound;
       this.stewieImage;
       this.quagmireImage;
+      this.youWonImage;
       this.stewieSound;
       this.megCounter = 0;
       this.player = new Player(500,300)
       this.lives = 3;
-      this.score = 0;
+      this.timer = 5;
         }
   
     preload() {
       this.backgroundImage = loadImage("assets/griffins-livingroom.png");
       this.peterImage = loadImage("assets/peter-griffin-little.png");
+      this.youWonImage = loadImage("assets/you-won.png");
       this.megImage = loadImage("assets/meg-griffin-little.png");
       this.stewieImage = loadImage("assets/stewie-griffin-little.png");
+      this.quagmireImage = loadImage("assets/quagmire-little.png");
       this.peterLife = loadImage("assets/peter-life.png");
       this.peterLaugh = createAudio('assets/peter-griffin-hehe.mp3');
       this.quagmireSound = createAudio('assets/quagmire-giggity.mp3');
@@ -28,6 +31,7 @@ class Game {
 
     draw() {
 
+        
 
       image(this.backgroundImage, 0, 0, width, height);
       // Play the peterLaugh sound
@@ -37,8 +41,23 @@ class Game {
       if (keyIsPressed && key === 's') {
         this.peterLaugh.stop();
       }
+
+      textAlign(CENTER, CENTER); 
+        textSize(100);
+        text(this.timer, width/5 * 4.7, height/12);
+
+
       
       //COUNTDOWN TIMER
+if (frameCount % 60 == 0 && this.timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+   this.timer --;
+  }
+  if (this.timer == 0) {
+    image(this.quagmireImage, 600, 200, 400, 500);
+    this.quagmireSound.play();
+    noLoop();
+}
+  
 
       
       
@@ -82,17 +101,11 @@ class Game {
   
           checkLives() {
         if (this.lives <= 0) {
-           // clearInterval(countdownInterval);
             noLoop();
             image(game.stewieImage, width/2 - 250, height/2 - 250, 500, 500);
-            let textX = width/2 - this.stewieImage.width/2;
-            let textY = height/2 - this.stewieImage.height/2 -60;
-            textAlign(CENTER);
-            text("YOU LOST!", textX, textY);            
-            fill('#FFFF00');
-            stroke('#000000');
-            strokeWeight(7);
-            textSize(80);
+            image(game.stewieImage, width/2 - 250, height/2 - 250, 500, 500);
+
+
             this.stewieSound.play();
 
          }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const game = new Game()
-let timeLimit = 90;
-let countDown; //time limit - time passed
+//let timeLimit = 90;
+// let countDown; //time limit - time passed
 
 // Load game assets
 function preload() {
@@ -20,17 +20,17 @@ function setup() {
 }
 
 function draw() {
-    let currentTime = int(millis() / 1000);
-    countDown = timeLimit - currentTime;
+    //let currentTime = int(millis() / 1000);
+    //countDown = timeLimit - currentTime;
 
-    if(countDown < 0){
-countDown = 0;
+  //  if(countDown < 0){
+//countDown = 0;
 
 
-    }
-    textSize(24)
-    fill("#000000")
-    text("Time: " + currentTime, 30, 380) 
+    //}
+    //textSize(24)
+    //fill("#000000")
+   // text("Time: " + currentTime, 30, 380) 
    // image(game.peterImage, mouseX, mouseY)
   game.draw();
 }
