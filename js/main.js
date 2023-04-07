b1958ce9dc5850334a18406b558cc61a3daf16fb
Diana Kalstein <dianakalstein@gmail.com>
2023-04-07 09:13:35 +0200

    Removed comments from main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 const game = new Game();
-//let timeLimit = 90;
-// let countDown; //time limit - time passed
 
 // Load game assets
 function preload() {
@@ -12,22 +10,11 @@ function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   frameRate(60);
 
-  //canvas.parent('canvas');
-  //textSize(40);
+
   noCursor();
 }
 
 function draw() {
-  //let currentTime = int(millis() / 1000);
-  //countDown = timeLimit - currentTime;
-
-  //  if(countDown < 0){
-  //countDown = 0;
-
-  //}
-  //textSize(24)
-  //fill("#000000")
-  // text("Time: " + currentTime, 30, 380)
-  // image(game.peterImage, mouseX, mouseY)
+  
   game.draw();
 }
